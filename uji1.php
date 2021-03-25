0a58117326ff1a3aaeba08261cfb5eee0a813be6
hernism29 <48180365+hernism29@users.noreply.github.com>
2021-03-25 21:48:24 +0700

    Update uji1.php

--- uji1.php
+++ uji1.php
@@ -13,25 +13,28 @@ $loop=count($dataset);
 $a = rand(0, 1);
 $b = rand(0, 1);
 for($i=0;$i<count($dataset);$i++){
-
-	for($j=0;$j<2;$j++){
+	for($j=0;$j<1;$j++){
 		echo "<br/>";
 		echo "<br/>";
 		echo "dataset [".$i."][".$j."] = ".$dataset[$i][$j];
-		echo "<br/>";
-		
-		 
-		echo "<br/>";
-		
+		echo "<br/>";	 
 		
 		// hitung
 		//kolom ke satu maka indeks ke 0 hitungnya
 		echo "hitung ".$a."+".$b."*".$dataset[$i][0]."=";
-		echo $a + $b * $dataset[$i][0];
+		echo $Y_predict = $a + $b * $dataset[$i][0];
+		echo "</br>";
+		echo "</br>";
 
 	}
-
+	echo "dataset [".$i."][".$j."] = ".$dataset[$i][1];
+	echo "</br>";
+	echo "hitung SSE : 0.5 *".$Y_predict. "-" .$dataset[$i][1]."^ 2 =";
+$sse = 0.5 * pow($Y_predict - $dataset[$i][1], 2);
+echo $sse;
 }
+
+?>
 
 ?>
 
